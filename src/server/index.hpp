09d5318f10b068ca0c5593f2b2bb8091268a5bb5
manuelwestermeier <westermeier111@gmail.com>
2025-06-03 22:12:16 +0200

    formatted

--- src/server/index.hpp
+++ src/server/index.hpp
@@ -6,7 +6,7 @@ struct Server
 {
     GitHubClient gh;
 
-    void init(const String &GITHUB_TOKEN = "", int port=0)
+    void init(const String &GITHUB_TOKEN = "", int port = 0)
     {
         gh = initGH(GITHUB_TOKEN);
     }
