ac7a4357c5159f69d15f5b105e5011c12c9edaca
manuelwestermeier <westermeier111@gmail.com>
2025-06-03 19:37:19 +0200

    restart

--- js/name-stystem/crypto.js
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-
-export function generateKey() {
-  return crypto.randomBytes(32).toString("hex"); // 256-bit
-}
-
-export function hash256(input) {
-  return crypto.createHash("sha256").update(input).digest("hex");
-}
