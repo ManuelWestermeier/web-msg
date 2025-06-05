6d5f4be6957b8bd89d3b8f39682323f84fb4dad7
manuelwestermeier <westermeier111@gmail.com>
2025-06-05 20:47:32 +0200

    add ip setter

--- src/server/index.hpp
+++ src/server/index.hpp
@@ -3,7 +3,6 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include "./init.hpp"
-
 #include "../get-public-ip.hpp"
 
 struct WebMsgServer
@@ -11,18 +10,55 @@ struct WebMsgServer
     GitHubClient gh;
     AsyncWebServer server;
     int serverPort;
+    String ip = "";
 
     WebMsgServer() : WebMsgServer(80) {}
     WebMsgServer(int port) : server(port), serverPort(port) {}
 
+    static void updateIpTask(void *param)
+    {
+        WebMsgServer *self = static_cast<WebMsgServer *>(param);
+        if (!self)
+            vTaskDelete(NULL); // safety
+
+        while (1)
+        {
+            String newIp = getPublicIP();
+
+            if (newIp.startsWith("Error:"))
+                continue;
+
+            if (newIp.length() > 0 && newIp != self->ip)
+            {
+                Serial.println("[updateIpTask] New IP: " + newIp);
+                self->gh.writeFile("ip.txt", newIp);
+                self->ip = newIp;
+            }
+
+            vTaskDelay(pdMS_TO_TICKS(60000)); // 60 Sekunden
+        }
+    }
+
     void init(const String &GITHUB_TOKEN = "")
     {
-        Serial.println("http://" + WiFi.localIP().toString() + (serverPort == 80 ? "" : +":" + String(serverPort)));
+        Serial.println("http://" + WiFi.localIP().toString() + (serverPort == 80 ? "" : ":" + String(serverPort)));
+
         gh = initGH(GITHUB_TOKEN);
 
-        server.on("/send", HTTP_GET, [](AsyncWebServerRequest *request) { //
-            request->send(200, "text/plain", getPublicIP());
-        });
+        server.on("/send", HTTP_GET, [this](AsyncWebServerRequest *request)
+                  { request->send(200, "text/plain", ip); });
+
+        BaseType_t result = xTaskCreate(
+            updateIpTask,
+            "UpdateIPTask",
+            8192,
+            this,
+            1,
+            NULL);
+        if (result != pdPASS)
+        {
+            Serial.println("❌ Failed to create updateIpTask");
+        }
 
         server.begin();
     }
